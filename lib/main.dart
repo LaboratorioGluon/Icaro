b6575faba64ed0e4a6709024840f519015801e31
Andy46 <agambmel@gmail.com>
2025-05-11 14:48:21 +0200

    Move camera page widget to own directory

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 // import 'package:provider/provider.dart';
 
-import 'camerapage.dart';
+import 'camerapage/camerapage.dart';
 import 'homepage.dart';
 import 'logspage.dart';
 import 'lorapage.dart';
