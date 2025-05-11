81f9215a5a1585a987c7fce33773d9471d8af868
Andy46 <agambmel@gmail.com>
2025-05-11 19:33:52 +0200

    Add button to manually manage navigation panel extension

--- lib/main.dart
+++ lib/main.dart
@@ -36,118 +36,164 @@ class IcaroMain extends StatefulWidget {
 }
 
 class _IcaroMainState extends State<IcaroMain> {
-    // ignore: unused_field
-    static const appTitle = Text("Project Icaro");
+  // ignore: unused_field
+  static const appTitle = Text("Project Icaro");
 
-    int selectedIndex = 0;
-    void updateIndex (int newIndex)
-    {
-        setState(() {selectedIndex = newIndex;});
+  final GlobalKey navKey = GlobalKey();
+  bool isExtended = true;
+  var buttonLeft = 0.0;
+  var buttonBottom = 0.0;
+
+  int selectedIndex = 2;
+  void updateIndex (int newIndex)
+  {
+      setState(() {selectedIndex = newIndex;});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    final style = theme.textTheme.displayMedium!.copyWith(
+      color: theme.colorScheme.onPrimary,
+    );
+
+    Widget page;
+    switch(selectedIndex){
+      case 0:
+        page = IcaroHomePage();
+      case 1:
+        page = IcaroSensorsPage();
+      case 2:
+        page = IcaroCameraPage();
+      case 3:
+        page = IcaroMapPage();
+      case 4:
+        page = IcaroPowerPage();
+      case 5:
+        page = IcaroLoraPage();
+      case 6:
+        page = IcaroMAVPage();
+      case 7:
+        page = IcaroLogsPage();
+      case 8:
+        page = IcaroSettingsPage();
+      default:
+      throw UnimplementedError("No widget for $selectedIndex");
     }
+      
+    var layout = LayoutBuilder(
+      builder: (context, constraints) {
+          var destinations = [
+              NavigationRailDestination(
+                icon: Icon(Icons.home),
+                label: Text('Home'),
+              ),
+              NavigationRailDestination(
+                icon: Icon(Icons.sensors),
+                label: Text('Sensors'),
+              ),
+              NavigationRailDestination(
+                icon: Icon(Icons.camera_alt),
+                label: Text('Camera'),
+              ),
+              NavigationRailDestination(
+                icon: Icon(Icons.map),
+                label: Text('Map'),
+              ),
+              NavigationRailDestination(
+                icon: Icon(Icons.power),
+                label: Text('Power'),
+              ),
+              NavigationRailDestination(
+                icon: Icon(Icons.wifi),
+                label: Text('Lora'),
+              ),
+              NavigationRailDestination(
+                icon: Icon(Icons.cell_wifi),
+                label: Text('MAV'),
+              ),
+              NavigationRailDestination(
+                icon: Icon(Icons.file_present),
+                label: Text('Logs'),
+              ),
+              NavigationRailDestination(
+                icon: Icon(Icons.settings),
+                label: Text('Settings'),
+              ),
+            ];
 
-    @override
-    Widget build(BuildContext context) {
-        Widget page;
-        switch(selectedIndex){
-          case 0:
-            page = IcaroHomePage();
-          case 1:
-            page = IcaroSensorsPage();
-          case 2:
-            page = IcaroCameraPage();
-          case 3:
-            page = IcaroMapPage();
-          case 4:
-            page = IcaroPowerPage();
-          case 5:
-            page = IcaroLoraPage();
-          case 6:
-            page = IcaroMAVPage();
-          case 7:
-            page = IcaroLogsPage();
-          case 8:
-            page = IcaroSettingsPage();
-          default:
-          throw UnimplementedError("No widget for $selectedIndex");
-        }
-        
-        var layout = LayoutBuilder(
-            builder: (context, constraints) {
-                var destinations = [
-                    NavigationRailDestination(
-                      icon: Icon(Icons.home),
-                      label: Text('Home'),
-                    ),
-                    NavigationRailDestination(
-                      icon: Icon(Icons.sensors),
-                      label: Text('Sensors'),
-                    ),
-                    NavigationRailDestination(
-                      icon: Icon(Icons.camera_alt),
-                      label: Text('Camera'),
-                    ),
-                    NavigationRailDestination(
-                      icon: Icon(Icons.map),
-                      label: Text('Map'),
-                    ),
-                    NavigationRailDestination(
-                      icon: Icon(Icons.power),
-                      label: Text('Power'),
-                    ),
-                    NavigationRailDestination(
-                      icon: Icon(Icons.wifi),
-                      label: Text('Lora'),
-                    ),
-                    NavigationRailDestination(
-                      icon: Icon(Icons.cell_wifi),
-                      label: Text('MAV'),
-                    ),
-                    NavigationRailDestination(
-                      icon: Icon(Icons.file_present),
-                      label: Text('Logs'),
-                    ),
-                    NavigationRailDestination(
-                      icon: Icon(Icons.settings),
-                      label: Text('Settings'),
-                    ),
-                  ];
+          void toggleRail() {
+            setState(() {
+              isExtended = !isExtended && (constraints.maxWidth >= 600);
+            });
+          }
 
-                var nav = SingleChildScrollView(
-                            child: ConstrainedBox(
-                              constraints: BoxConstraints(minHeight: MediaQuery.of(context).size.height),
-                                child: IntrinsicHeight(
-                                  child: NavigationRail(
-                                    extended: constraints.maxWidth >= 800,
-                                    selectedIndex: selectedIndex,
-                                    onDestinationSelected: (int index) {
-                                      updateIndex(index);
-                                    },
-                                    destinations: destinations,
-                                  ),
-                                ),
-                              ),
-                            );
+          var nav = SingleChildScrollView(
+            key: navKey,
+            child: ConstrainedBox(
+              constraints: BoxConstraints(minHeight: MediaQuery.of(context).size.height),
+                child: IntrinsicHeight(
+                  child: NavigationRail(
+                    extended: isExtended,
+                    selectedIndex: selectedIndex,
+                    onDestinationSelected: (int index) {
+                      updateIndex(index);
+                    },
+                    destinations: destinations,
+                  ),
+                ),
+            ),
+          );
 
-                var center = Expanded(
-                                child: Container(
-                                    color: Theme.of(context).colorScheme.primaryContainer,
-                                    child: page,
-                                ),
-                              );
+          var center = Expanded(
+            child: Container(
+                color: Theme.of(context).colorScheme.primaryContainer,
+                child: page,
+            ),
+          );
 
-                return Scaffold(
-                    body: Row(
-                        children: [
-                            nav,
-                            VerticalDivider(thickness: 1, width: 1),
-                            center,
-                        ],
-                    ),
-                );
-            },
+          var extendButton = FloatingActionButton(
+            onPressed: toggleRail,
+            mini: true,
+            child: Icon(isExtended ? Icons.arrow_back : Icons.arrow_forward),
+          );
+
+          // Position extendedbutton
+          WidgetsBinding.instance.addPostFrameCallback((_) {
+            final navContext = navKey.currentContext;
+            if (navContext != null) {
+              setState(() {
+                final box = navContext.findRenderObject() as RenderBox;
+                final size = box.size;
+                buttonLeft = size.width - 20.0;
+                buttonBottom = 20.0;
+                isExtended = isExtended && (constraints.maxWidth >= 600);
+              });
+            }
+          });
+
+        return Scaffold(
+          body: Stack(
+            children: [
+              Row(
+                children: [
+                  nav,
+                  VerticalDivider(thickness: 1, width: 1),
+                  center,
+                ]
+              ),
+              Positioned(
+                left: buttonLeft,
+                bottom: buttonBottom,
+                child: extendButton,
+              )
+            ],
+          ),
         );
+      },
+    );
 
-        return layout;
-    }
+    return layout;
+  }
 }
 
